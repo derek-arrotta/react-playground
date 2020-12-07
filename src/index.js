37a21244860df843105c1bcbea5fc6633832ff35
Derek Arrotta <derek.arrotta@gmail.com>
2020-12-07 11:34:57 -0500

    context excercise

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './paltrows-power-toes/App';
 import './index.css';
-import { BrowserRouter } from "react-router-dom";
+import AppLang from './lang-context/AppLang';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, 
+  <AppLang />,
   document.getElementById('root')
 );
